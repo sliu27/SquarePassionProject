34c85e70c4240ab57a05e9d26916a5adc96640a2
aschugart <aschugart@college.harvard.edu>
2017-08-17 23:09:38 -0400

    rerouted login

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get "/status" => "pages#status"
   get "/searchresults" => "pages#searchresults"
   devise_for :users
+  devise_scope :user do
+  get 'login', to: 'devise/sessions#new'
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
