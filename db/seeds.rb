b1529e3d01f3293bb357d89f1d498f602504e8ea
sliu27 <susan_liu@brown.edu>
2017-08-18 10:32:50 -0400

    css layout stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-shelters = Shelter.create([{ name: 'Odyssey Villas Intact Families', description: "We are a nice place", requirements: "all genders, all ages", 
+shelters = Shelter.create([{ name: 'Odyssey Villas Intact Families', description: "The Office of Emergency and Transitional Housing is comprised of an Assessment Center, Supportive Services and Transitional Housing programs that provide support and structure for male, women and children relearning and developing life skills essential for securing stable employment and independent living. ", requirements: "all genders, all ages", 
 	zipcode: "30318", address:"625 Spencer Street NW, Atlanta, GA 30318" }, 
-	{name: 'The Salvation Army Metro Atlanta Red Shield Service', description: "We are SA", requirements: "all genders, all ages", 
+	{name: 'The Salvation Army Metro Atlanta Red Shield Service', description: "The purpose of The Salvation Army Metro Atlanta Red Shield Services Emergency and Transitional Housing Facility (Red Shield Shelter) is to provide strategic intervention, refuge and safe lodging for homeless men, women and families with children in crisis. ", requirements: "all genders, all ages", 
 	zipcode: "30313", address:"469 Marietta Street, Atlanta, GA 30313"},
-	{name: 'Serenity House of Atlanta', description: "We are SA", requirements: "all genders, all ages", 
+	{name: 'Serenity House of Atlanta', description: "Serenity House of Atlanta Ministries is an 501c non-profit organization that provides housing and comprehensive support services to the homeless and people at risk in order to facilitate changes in their lives.", requirements: "all genders, all ages", 
 	zipcode: "30314", address:"Atlanta, GA, 30314"},
 	{name: 'The Shepherds Inn Downtown Atlanta Homeless Shelter', description: "Every day, we serve up to 450 men with the goal of shepherding them, one at a time, into a new life of independent living.", 
 		requirements: "Men, 18 +", zipcode: "30313", address:"165 Ivan Allen Blvd NW, Atlanta, GA 30313"},
@@ -22,6 +22,7 @@ shelters = Shelter.create([{ name: 'Odyssey Villas Intact Families', description
 		requirements: "Men, 18 +", zipcode: "30303", address:"275 Pryor St., SW, Atlanta, GA 30303"},
 	
 	])
+
 
 
 # var shelterNames: [String] = ["Odyssey Villas Intact Families", "The Salvation Army Metro Atlanta Red Shield Service", 
